a4ef30082c575ea742a128bd515c4d2dbea729fe
Antoon <Antoon.Kragten@Softbrick.nl>
2020-07-29 08:24:06 +0200

    compile

--- dist/providers/emptyaction.js
+++ dist/providers/emptyaction.js
@@ -52,7 +52,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.EmptyAction = void 0;
 var action_1 = require("../action");
 var db_1 = __importDefault(require("../db"));
 var util_1 = require("../util");
--- dist/providers/retourtermijn.js
+++ dist/providers/retourtermijn.js
@@ -13,7 +13,6 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Retourtermijn = void 0;
 var crud_1 = require("../crud");
 var dict = {
     table: "RETOURTERMIJN",
